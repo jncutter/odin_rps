33369c56abfdaf1c82a409d5e3aa18f472f4b899
Jeremy <175655267+jncutter@users.noreply.github.com>
2024-08-13 15:14:50 -0500

    Add script.js with choice function

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let num = Math.random();
+    if (num < 0.33) {
+        return "Rock";
+    }
+    else if (num > 0.33 && num < 0.66) {
+        return "Paper";
+    }
+    else return "Scissors";
+}
