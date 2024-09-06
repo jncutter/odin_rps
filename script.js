6a848dd8ff2281af044e846ce078700097477ae0
Jeremy <175655267+jncutter@users.noreply.github.com>
2024-09-06 14:13:59 -0500

    Remove game function and add button functionality
    Clicking the buttons will now start a round of rps using the label
    on the button as the player choice.

--- script.js
+++ script.js
@@ -9,83 +9,73 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getPlayerChoice() {
-    let playerChoice = window.prompt("Rock, paper, or scissors?");
-    return playerChoice.toLowerCase();
-}
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame() {
-    for (let i=0; i<5; i++) {
-        function playRound(playerChoice, computerChoice) {
-            const playerSelection = getPlayerChoice();
-            const computerSelection = getComputerChoice();
+function playRound(playerChoice) {
+    const playerSelection = playerChoice;
+    const computerSelection = getComputerChoice();
             
-            if (playerSelection === computerSelection) {
-                console.log("You both picked " + playerSelection + ". This round is a draw, try again.");
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            } 
-            else if (playerSelection === "rock" && computerSelection === "paper") {
-                console.log("You picked rock, and the computer picked paper. You lose, try again.");
-                computerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-            else if (playerSelection === "rock" && computerSelection === "scissors") {
-                console.log("You picked rock, and the computer picked scissors. You win! Try again.");
-                playerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-        
-            else if (playerSelection === "paper" && computerSelection === "rock") {
-                console.log("You picked paper, and the computer picked rock. You win! Try again.");
-                playerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-        
-            else if (playerSelection === "paper" && computerSelection === "scissors") {
-                console.log("You picked paper, and the computer picked scissors. You lose, try again.");
-                computerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-        
-            else if (playerSelection === "scissors" && computerSelection === "rock") {
-                console.log("You picked scissors, and the computer picked rock. You lose, try again.");
-                computerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-        
-            else if (playerSelection === "scissors" && computerSelection === "paper") {
-                console.log("You picked scissors, and the computer picked paper. You win! Try again.");
-                playerScore++;
-                console.log("You: " + playerScore);
-                console.log("Computer: " + computerScore);
-            }
-        
-            else {
-                console.log("That is not a valid response, please try again.");
-                i--;
-            }
+        if (playerSelection === computerSelection) {
+            console.log("You both picked " + playerSelection + ". This round is a draw, try again.");
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        } 
+        else if (playerSelection === "rock" && computerSelection === "paper") {
+            console.log("You picked rock, and the computer picked paper. You lose, try again.");
+            computerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
         }
-        playRound();
-    }
-    if (playerScore == computerScore) {
-        console.log("This game ends in a draw.");
-    }
-    else if (playerScore > computerScore) {
-        console.log("You win! Good job.");
-    }
-    else if (playerScore < computerScore) {
-        console.log("The computer wins. Better luck next time.");
-    }
-    console.log("Refresh the page to play again.");
+        else if (playerSelection === "rock" && computerSelection === "scissors") {
+            console.log("You picked rock, and the computer picked scissors. You win! Try again.");
+            playerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        }
+        
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            console.log("You picked paper, and the computer picked rock. You win! Try again.");
+            playerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        }
+        
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            console.log("You picked paper, and the computer picked scissors. You lose, try again.");
+            computerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        }
+        
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            console.log("You picked scissors, and the computer picked rock. You lose, try again.");
+            computerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        }
+        
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            console.log("You picked scissors, and the computer picked paper. You win! Try again.");
+            playerScore++;
+            console.log("You: " + playerScore);
+            console.log("Computer: " + computerScore);
+        }
+        
+        else {
+            console.log("That is not a valid response, please try again.");
+            i--;
+        }
 }
 
-playGame();
+document.getElementById("rock").addEventListener("click", () => {
+    playRound("rock");
+});
+
+document.getElementById("paper").addEventListener("click", () => {
+    playRound("paper");
+});
+
+document.getElementById("scissors").addEventListener("click", () => {
+    playRound("scissors");
+});
